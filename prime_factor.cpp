b6b00df4caecfd7bb61a8886f4c6fe89a11e5a8b
Abhishek Singh <abesingh1@gmail.com>
2024-02-22 10:28:35 +0530

    prime factorization

--- prime_factor.cpp
+++ prime_factor.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 bool isPrime(int number) {
-    if (number <= 1)
-    {
-        return false;
-    }
-    
     for (int i = 2; i < sqrt(number); i++)
     {
         if (number % i == 0)
@@ -16,4 +13,26 @@ bool isPrime(int number) {
         
     }
     return true;
+}
+
+void primeFactorization(int number) {
+    for (int i = 2; i <= number; i++)
+    {
+        if (isPrime(i))
+        {
+            while (number % i == 0)
+            {
+                cout << i << " ";
+                number /= i;
+            }
+        }
+    }
+}
+
+int main() {
+    int input;
+    cout << "Enter a number";
+    cin >> input;
+    primeFactorization(input);
+    return 0;
 }
