7d6d7d14db6e9bda4648684338498d5cd19c3aef
Abhishek Singh <abesingh1@gmail.com>
2024-02-24 11:18:36 +0530

    is array sorted

---
+++ is_array_sorted.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+bool isAscending(int a, int b) {
+    return a < b;
+}
+
+bool isDescending(int a, int b) {
+    return a > b;
+}
+
+bool isArraySorted(int arr[], bool (*sortingFunction)(int, int)) {
+    for (int i = 1; i < (sizeof(arr)/sizeof(arr[0])); i++)
+    {
+        if (sortingFunction(arr[i], arr[i-1]))
+        {
+            return false;
+        }
+        
+    }
+    return true;
+}
+
+int main() {
+    int t;
+    int sortingOrder;
+    cout << "Enter array size\n";
+    cin >> t;
+    int arr[t];
+    int index = 0;
+    cout << "Enter array element one by one\n";
+    while (index < t) {
+        int element;
+        cin >> element;
+        arr[index] = element;
+        index++;
+    }
+    cout << "Enter sorting order: 1 for ascending, 2 for descending\n";
+    cin >> sortingOrder;
+    cout << std::boolalpha << isArraySorted(arr, sortingOrder == 1 ? &isAscending : &isDescending) << endl;
+}
