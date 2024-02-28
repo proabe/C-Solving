68de59bcd876820823e712be06aceca800d9784d
Abhishek Singh <abesingh1@gmail.com>
2024-02-28 10:38:09 +0530

    2d array using double pointer, passing it to a function

---
+++ 2d_array_using_double_pointer.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void print2dArray(int **arr, int m, int n) {
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    int m, n;
+    cout << "Enter no. of rows of the array\n";
+    cin >> m;
+    cout << "Enter no. of columns of the array\n";
+    cin >> n;
+    int **arr;
+    arr = new int *[m];
+    for (int i = 0; i < m; i++)
+    {
+        arr[i] = new int [n];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = 101;
+        }
+    }
+    print2dArray(arr, m, n);
+    return 0;
+}
