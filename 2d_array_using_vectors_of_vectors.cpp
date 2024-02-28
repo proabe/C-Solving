352def7fd07748ed32e31a11b3697f5cfac93aa2
Abhishek Singh <abesingh1@gmail.com>
2024-02-28 11:21:04 +0530

    2d array using vectors of vectors

--- 2d_array_using_vectors_of_vectors.cpp
+++ 2d_array_using_vectors_of_vectors.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 // advantages:
-// numbers of columns can be dynamic
+// numbers of rows and columns can be dynamic
 // easier to pass as an argument to function
 
-void print2dArray(vector<int> arr[], int m) {
-    for (int i = 0; i < m; i++)
+void print2dArray(vector< vector<int> > arr) {
+    for (int i = 0; i < arr.size(); i++)
     {
         for (int j = 0; j < arr[i].size(); j++)
         {
@@ -23,14 +23,16 @@ int main() {
     cin >> m;
     cout << "Enter no. of columns of the array\n";
     cin >> n;
-    vector<int> arr[m];
+    vector< vector<int> > arr;
     for (int i = 0; i < m; i++)
     {
+        vector <int> v;
         for (int j = 0; j < n; j++)
         {
-            arr[i].push_back(101);
+            v.push_back(101);
         }
+        arr.push_back(v);
     }
-    print2dArray(arr, m);
+    print2dArray(arr);
     return 0;
 }
